bbac4551e5df064e3bc55a71c78ea7d30022ce46
Geoffrey <mv.geoffrey.aaron@gmail.com>
2021-04-10 00:29:43 +0800

    chore: scroll change size

--- rust_webgl/index.js
+++ rust_webgl/index.js
@@ -1,13 +1,26 @@
 const rust = import('./rust_webgl')
-let value =10;
-console.log(rust.then(data=>data.start(value)))
+let value = 1;
+console.log(rust.then(data => data.start(value)))
 const canvas = document.getElementById("init");
-canvas.width =window.screen.width;
-canvas.height = window.screen.height;
-document.getElementById("init").addEventListener("wheel",async (event)=>{
-  value=value+1;
-  console.log(value);
-  
-  console.log(await rust.then(data=>data.start(value)))
+canvas.width = window.screen.width-30;
+canvas.height = window.screen.height-140;
+document.getElementById("init").addEventListener("wheel", async (event) => {
+  if (event.deltaY > 0) {
+    value = value + 1;
+  }
+  if (event.deltaY < 0) {
+    if (value > 10) {
+      value = value - 1;
+    }
+  }
+  const test =await rust.then(data => data.start(value))
+  console.log(test);
 });
 
+// document.getElementById("init").addEventListener('gestureend', function (e) {
+//   if (e.scale < 1.0) {
+//     console.log( 'User moved fingers closer together');
+//   } else if (e.scale > 1.0) {
+//     console.log( 'User moved fingers further apart');
+//   }
+// }, false);
